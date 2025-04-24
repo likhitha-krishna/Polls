4b4425b2cb02509b3b87d117211bea5c252ebe36
likhitha-krishna <likhitha0622@gmail.com>
2025-04-24 14:50:08 +0530

    added base dir as i don't have static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = 'static/'
 
 if not DEBUG:
